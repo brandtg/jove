7fcc142809ad1e716d0d67c26641bc933942666b
Greg Brandt <brandt.greg@gmail.com>
2025-01-11 10:41:35 -0800

    Update setup.py with metadata

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
 from setuptools import setup, find_packages
 
+with open("README.md") as f:
+    long_description = f.read()
+
 setup(
     name="jove",
-    version="0.0.1",
+    version="0.0.2",
+    author="Greg Brandt",
+    author_email="brandt.greg@protonmail.com",
+    description="A terminal-focused alternative to Jupyter notebooks.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/brandtg/jove",
     packages=find_packages(),
     install_requires=[
         # TODO Anything?
